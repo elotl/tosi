5c24659556a73e8657ab0be5de0eb4015a488672
Vilmos Nebehaj <vilmos@elotl.co>
2018-06-21 13:04:20 -0700

    Add new test case for k8s.gcr.io

--- scripts/run_tests.sh
+++ scripts/run_tests.sh
@@ -27,6 +27,7 @@ echo -n > /tmp/tosi.log
 ./tosi -image bitnami/tomcat:8.5.31 >> /tmp/tosi.log 2>&1
 ./tosi -image library/nginx:1.7.9 >> /tmp/tosi.log 2>&1
 ./tosi -image library/nginx:latest >> /tmp/tosi.log 2>&1
+./tosi -image k8s.gcr.io/k8s-dns-kube-dns-amd64:1.14.10 >> /tmp/tosi.log 2>&1
 
 ./tosi -image library/alpine:3.6 >> /tmp/tosi.log 2>&1
 compare_rootfs "/tmp/tosi/packages/library-alpine-3.6/3.6/ROOTFS"
