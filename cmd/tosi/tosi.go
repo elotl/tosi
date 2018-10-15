0252e1b2406a90f57fb5f5aca207125fed70c5b3
Vilmos Nebehaj <vilmos@elotl.co>
2018-10-15 14:27:17 -0700

    Create directory for files in tarball if necessary

--- cmd/tosi/tosi.go
+++ cmd/tosi/tosi.go
@@ -524,6 +524,10 @@ func extractLayerToDir(filename, destdir string) ([]string, error) {
 				glog.Errorf("Extracting %s: read %d bytes, but size is %d bytes",
 					name, read_so_far, header.Size)
 			}
+			dir := filepath.Dir(name)
+			if _, err := os.Stat(dir); os.IsNotExist(err) {
+				os.MkdirAll(dir, 0755)
+			}
 			ioutil.WriteFile(name, data, mode)
 			err = os.Chmod(name, mode)
 			if err != nil {
