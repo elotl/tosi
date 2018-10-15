9d1a6c3ac663f5749069e36f010f8c618ccd9b09
Vilmos Nebehaj <vilmos@elotl.co>
2018-10-15 13:20:05 -0700

    Check git diff-index

--- scripts/run_tests.sh
+++ scripts/run_tests.sh
@@ -5,6 +5,9 @@ set -e
 SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 ROOT_DIR=$SCRIPT_DIR/..
 cd $ROOT_DIR
+echo ">>> git diff-index:"
+git diff-index --quiet HEAD
+echo ">>> end of git diff-index"
 make
 go test ./...
 
