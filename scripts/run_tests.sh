17f55b73e8e74d54df7a84649ae1db242dbd4a2f
Vilmos Nebehaj <v.nebehaj@gmail.com>
2020-05-01 10:41:49 -0700

    Fix tests

--- scripts/run_tests.sh
+++ scripts/run_tests.sh
@@ -6,41 +6,23 @@ SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 ROOT_DIR=$SCRIPT_DIR/..
 cd $ROOT_DIR
 
-compare_rootfs() {
-    rootfs="$1"
-    pushd "$rootfs"
-    find . -exec ls -d -l {} \; | awk '{ s = ""; for (i = 9; i <= NF; i++) s = s $i " "; print s }' | sort > /tmp/files1.txt
-    cd ..
-    mkdir -p check
-    cd check
-    tar xzf ../*.tar.gz
-    cd ROOTFS
-    find . -exec ls -d -l {} \; | awk '{ s = ""; for (i = 9; i <= NF; i++) s = s $i " "; print s }' | sort > /tmp/files2.txt
-    cmp /tmp/files1.txt /tmp/files2.txt
-    popd
-}
-
 echo -n > /tmp/tosi.log
 
-./tosi -image bitnami/tomcat:8.5.31 >> /tmp/tosi.log 2>&1
-./tosi -image library/nginx:1.7.9 >> /tmp/tosi.log 2>&1
-./tosi -image library/nginx:latest >> /tmp/tosi.log 2>&1
-./tosi -image library/alpine@sha256:6a92cd1fcdc8d8cdec60f33dda4db2cb1fcdcacf3410a8e05b3741f44a9b5998 >> /tmp/tosi.log 2>&1
-./tosi -image library/alpine:latest@sha256:6a92cd1fcdc8d8cdec60f33dda4db2cb1fcdcacf3410a8e05b3741f44a9b5998 >> /tmp/tosi.log 2>&1
-./tosi -image k8s.gcr.io/k8s-dns-kube-dns-amd64:1.14.10 >> /tmp/tosi.log 2>&1
+./tosi -image bitnami/tomcat:8.5.31 -saveconfig /tmp/config -extractto /tmp/rootfs >> /tmp/tosi.log 2>&1
+./tosi -image library/nginx:1.7.9 -saveconfig /tmp/config -extractto /tmp/rootfs >> /tmp/tosi.log 2>&1
+./tosi -image library/nginx:latest -saveconfig /tmp/config -extractto /tmp/rootfs >> /tmp/tosi.log 2>&1
+./tosi -image library/ruby -saveconfig /tmp/config -extractto /tmp/rootfs >> /tmp/tosi.log 2>&1
+./tosi -image library/alpine:3.6 -saveconfig /tmp/config -extractto /tmp/rootfs >> /tmp/tosi.log 2>&1
+./tosi -image library/alpine@sha256:6a92cd1fcdc8d8cdec60f33dda4db2cb1fcdcacf3410a8e05b3741f44a9b5998 -saveconfig /tmp/config -extractto /tmp/rootfs >> /tmp/tosi.log 2>&1
+./tosi -image library/alpine:latest@sha256:6a92cd1fcdc8d8cdec60f33dda4db2cb1fcdcacf3410a8e05b3741f44a9b5998 -saveconfig /tmp/config -extractto /tmp/rootfs >> /tmp/tosi.log 2>&1
+./tosi -image k8s.gcr.io/k8s-dns-kube-dns-amd64:1.14.10 -saveconfig /tmp/config -extractto /tmp/rootfs >> /tmp/tosi.log 2>&1
 # Manifest is v1+prettyjws.
-./tosi -image k8s.gcr.io/redis:e2e >> /tmp/tosi.log 2>&1
+./tosi -image k8s.gcr.io/redis:e2e -saveconfig /tmp/config -extractto /tmp/rootfs >> /tmp/tosi.log 2>&1
 # One layer contains a directory without creating parent directory first.
-./tosi -image jenkinsxio/jx:2.0.22 >> /tmp/tosi.log 2>&1
+./tosi -image jenkinsxio/jx:2.0.22 -saveconfig /tmp/config -extractto /tmp/rootfs >> /tmp/tosi.log 2>&1
 # Manifest that requires per-layer whiteouts.
-./tosi -url https://gcr.io -image google-samples/gb-frontend:v4 >> /tmp/tosi.log 2>&1
+./tosi -url https://gcr.io -image google-samples/gb-frontend:v4 -saveconfig /tmp/config -extractto /tmp/rootfs >> /tmp/tosi.log 2>&1
 # Registry that does not support pings.
-./tosi -url https://quay.io -image quay/redis >> /tmp/tosi.log 2>&1
+./tosi -url https://quay.io -image quay/redis -saveconfig /tmp/config -extractto /tmp/rootfs >> /tmp/tosi.log 2>&1
 # A layer creates a file that overwrites a symlink from a previous layer.
-./tosi -url https://gcr.io/ -image google-containers/conformance:v1.17.3 >> /tmp/tosi.log 2>&1
-
-./tosi -image library/alpine:3.6 >> /tmp/tosi.log 2>&1
-compare_rootfs "/tmp/tosi/packages/library-alpine-3.6/3.6/ROOTFS"
-
-./tosi -image library/ruby >> /tmp/tosi.log 2>&1
-compare_rootfs "/tmp/tosi/packages/library-ruby/latest/ROOTFS"
+./tosi -url https://gcr.io/ -image google-containers/conformance:v1.17.3 -saveconfig /tmp/config -extractto /tmp/rootfs >> /tmp/tosi.log 2>&1
