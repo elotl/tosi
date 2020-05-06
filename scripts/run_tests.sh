6cc7bee87a354d22a67a4f67d8c81f725f721d0a
Vilmos Nebehaj <v.nebehaj@gmail.com>
2020-05-06 15:32:10 -0700

    Add tests

--- scripts/run_tests.sh
+++ scripts/run_tests.sh
@@ -9,12 +9,16 @@ cd $ROOT_DIR
 tmpdir="$(mktemp -d)"
 
 function cleanup() {
+    for m in $(mount | grep "on $tmpdir" | awk '{print $3}'); do
+        umount "$m"
+    done
     rm -rf $tmpdir
 }
 trap cleanup EXIT
 
 echo -n > /tmp/tosi.log
 
+# Basic checks.
 ./tosi -image bitnami/tomcat:8.5.31 -saveconfig /tmp/config -extractto "$(mktemp -d -p $tmpdir)" >> /tmp/tosi.log 2>&1
 ./tosi -image library/nginx:1.7.9 -saveconfig /tmp/config -extractto "$(mktemp -d -p $tmpdir)" >> /tmp/tosi.log 2>&1
 ./tosi -image library/nginx:latest -saveconfig /tmp/config -extractto "$(mktemp -d -p $tmpdir)" >> /tmp/tosi.log 2>&1
@@ -33,3 +37,9 @@ echo -n > /tmp/tosi.log
 ./tosi -url https://quay.io -image quay/redis -saveconfig /tmp/config -extractto "$(mktemp -d -p $tmpdir)" >> /tmp/tosi.log 2>&1
 # A layer creates a file that overwrites a symlink from a previous layer.
 ./tosi -url https://gcr.io/ -image google-containers/conformance:v1.17.3 -saveconfig /tmp/config -extractto "$(mktemp -d -p $tmpdir)" >> /tmp/tosi.log 2>&1
+# Create overlayfs.
+rootfs="$(mktemp -d -p $tmpdir)"
+./tosi -image library/ubuntu -mount "$rootfs" >> /tmp/tosi.log 2>&1
+[ "$(ls -l $rootfs | wc -l)" -gt 5 ]
+# Use cached layers.
+./tosi -image library/ubuntu >> /tmp/tosi.log 2>&1
