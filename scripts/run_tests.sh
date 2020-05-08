e724901ca4b0dc2c26cec1ec4ba7b969d0061090
Vilmos Nebehaj <v.nebehaj@gmail.com>
2020-05-08 16:01:22 -0700

    Fix tests

--- scripts/run_tests.sh
+++ scripts/run_tests.sh
@@ -16,27 +16,37 @@ function cleanup() {
 }
 trap cleanup EXIT
 
+function tmpd() {
+    echo "$(mktemp -d -p $tmpdir)"
+}
+
+function tmpf() {
+    local f="$(mktemp -p $tmpdir)"
+    rm "$f"
+    echo $f
+}
+
 # Basic checks.
-./tosi -image bitnami/tomcat:8.5.31 -saveconfig /tmp/config -extractto "$(mktemp -d -p $tmpdir)"
-./tosi -image library/nginx:1.7.9 -saveconfig /tmp/config -extractto "$(mktemp -d -p $tmpdir)"
-./tosi -image library/nginx:latest -saveconfig /tmp/config -extractto "$(mktemp -d -p $tmpdir)"
-./tosi -image library/ruby -saveconfig /tmp/config -extractto "$(mktemp -d -p $tmpdir)"
-./tosi -image library/alpine:3.6 -saveconfig /tmp/config -extractto "$(mktemp -d -p $tmpdir)"
-./tosi -image library/alpine@sha256:6a92cd1fcdc8d8cdec60f33dda4db2cb1fcdcacf3410a8e05b3741f44a9b5998 -saveconfig /tmp/config -extractto "$(mktemp -d -p $tmpdir)"
-./tosi -image library/alpine:latest@sha256:6a92cd1fcdc8d8cdec60f33dda4db2cb1fcdcacf3410a8e05b3741f44a9b5998 -saveconfig /tmp/config -extractto "$(mktemp -d -p $tmpdir)"
-./tosi -image k8s.gcr.io/k8s-dns-kube-dns-amd64:1.14.10 -saveconfig /tmp/config -extractto "$(mktemp -d -p $tmpdir)"
+./tosi -image bitnami/tomcat:8.5.31 -saveconfig "$(tmpf)" -extractto "$(tmpd)"
+./tosi -image library/nginx:1.7.9 -saveconfig "$(tmpf)" -extractto "$(tmpd)"
+./tosi -image library/nginx:latest -saveconfig "$(tmpf)" -extractto "$(tmpd)"
+./tosi -image library/ruby -saveconfig "$(tmpf)" -extractto "$(tmpd)"
+./tosi -image library/alpine:3.6 -saveconfig "$(tmpf)" -extractto "$(tmpd)"
+./tosi -image library/alpine@sha256:6a92cd1fcdc8d8cdec60f33dda4db2cb1fcdcacf3410a8e05b3741f44a9b5998 -saveconfig "$(tmpf)" -extractto "$(tmpd)"
+./tosi -image library/alpine:latest@sha256:6a92cd1fcdc8d8cdec60f33dda4db2cb1fcdcacf3410a8e05b3741f44a9b5998 -saveconfig "$(tmpf)" -extractto "$(tmpd)"
+./tosi -image k8s.gcr.io/k8s-dns-kube-dns-amd64:1.14.10 -saveconfig "$(tmpf)" -extractto "$(tmpd)"
 # Manifest is v1+prettyjws.
-./tosi -image k8s.gcr.io/redis:e2e -saveconfig /tmp/config -extractto "$(mktemp -d -p $tmpdir)"
+./tosi -image k8s.gcr.io/redis:e2e -saveconfig "$(tmpf)" -extractto "$(tmpd)"
 # One layer contains a directory without creating parent directory first.
-./tosi -image jenkinsxio/jx:2.0.22 -saveconfig /tmp/config -extractto "$(mktemp -d -p $tmpdir)"
+./tosi -image jenkinsxio/jx:2.0.22 -saveconfig "$(tmpf)" -extractto "$(tmpd)"
 # Manifest that requires per-layer whiteouts.
-./tosi -url https://gcr.io -image google-samples/gb-frontend:v4 -saveconfig /tmp/config -extractto "$(mktemp -d -p $tmpdir)"
+./tosi -url https://gcr.io -image google-samples/gb-frontend:v4 -saveconfig "$(tmpf)" -extractto "$(tmpd)"
 # Registry that does not support pings.
-./tosi -url https://quay.io -image quay/redis -saveconfig /tmp/config -extractto "$(mktemp -d -p $tmpdir)"
+./tosi -url https://quay.io -image quay/redis -saveconfig "$(tmpf)" -extractto "$(tmpd)"
 # A layer creates a file that overwrites a symlink from a previous layer.
-./tosi -url https://gcr.io/ -image google-containers/conformance:v1.17.3 -saveconfig /tmp/config -extractto "$(mktemp -d -p $tmpdir)"
+./tosi -url https://gcr.io/ -image google-containers/conformance:v1.17.3 -saveconfig "$(tmpf)" -extractto "$(tmpd)"
 # Create overlayfs.
-rootfs="$(mktemp -d -p $tmpdir)"
+rootfs="$(tmpd)"
 ./tosi -image library/ubuntu -mount "$rootfs"
 [ "$(ls -l $rootfs | wc -l)" -gt 5 ]
 # Use cached layers.
