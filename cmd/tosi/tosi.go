f48fbe8405ef37f8fcf6915fdf659a22cac34582
Vilmos Nebehaj <v.nebehaj@gmail.com>
2020-06-17 11:02:42 -0700

    Don't log command line arguments

--- cmd/tosi/tosi.go
+++ cmd/tosi/tosi.go
@@ -20,6 +20,7 @@ import (
 	"flag"
 	"fmt"
 	"os"
+	"path/filepath"
 
 	"github.com/docker/docker/api/types/container"
 	"github.com/elotl/tosi/pkg/registryclient"
@@ -56,12 +57,17 @@ func main() {
 	flag.Parse()
 	flag.Lookup("logtostderr").Value.Set("true")
 
+	progname := "tosi"
+	if len(os.Args) > 0 {
+		progname = filepath.Base(os.Args[0])
+	}
+
 	if *version {
-		fmt.Printf("Tosi version %s\n", VERSION)
+		fmt.Printf("%s version %s\n", progname, VERSION)
 		os.Exit(0)
 	}
 
-	glog.Infof("Tosi version: %s parameters: %v", VERSION, os.Args)
+	glog.Infof("%s version: %s", progname, VERSION)
 
 	if *image == "" {
 		glog.Fatalf("Please specify image to pull")
